55f13e65cc5b2771ba04a3f892a9a77a03af284a
myundefined <35559578+myundefined@users.noreply.github.com>
2019-03-29 13:00:39 -0500

    Add files via upload

---
+++ mp3播放器/js/music.js
@@ -0,0 +1,14 @@
+var music = [
+  [
+    "体面",
+    "https://p3fx.kgimg.com/stdmusic/20171221/20171221090821129644.jpg",
+    "于文文",
+    "http://fs.w.kugou.com/201903292353/4b420920757eaa04d288144ca7bdb04b/G135/M06/17/13/xw0DAFtHOJOACZiNAEUe3eVFTx8339.mp3"
+  ],
+  [
+    "生僻字",
+    "https://p3fx.kgimg.com/stdmusic/20171115/20171115124042700588.jpg",
+    "陈柯宇",
+    "http://fs.w.kugou.com/201903292359/5003b0f4e67b1775befce6b2e2d29273/G111/M06/1D/10/D4cBAFoL9VyASCmXADTAFw14uaI428.mp3"
+  ]
+];
